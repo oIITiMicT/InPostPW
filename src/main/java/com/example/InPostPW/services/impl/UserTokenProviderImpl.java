a5de08d3b7b2e2aedbf2c2ad2b27b8c9d147da50
Eugene Batskalevich <backawork@gmail.com>
2022-12-07 09:31:05 +0100

    add refresh and access tokens

--- src/main/java/com/example/InPostPW/services/impl/UserTokenProviderImpl.java
+++ src/main/java/com/example/InPostPW/services/impl/UserTokenProviderImpl.java
@@ -9,27 +9,48 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import java.util.Date;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class UserTokenProviderImpl implements UserTokenProvider<String> {
+
+    private final UserServiceImpl userService;
+
     @Value("${jwt.refresh.token.duration_in_ms}")
     private String refreshTokenDurationMS;
+
+    @Value("${jwt.access.token.duration_in_ms}")
+    private String accessTokenDurationMS;
 
     @Value("${jwt.token.secret}")
     private String secretKey;
 
-    private final UserServiceImpl userService;
 
     @Override
-    public String provide(String username) {
+    public Map<String, String> provide(String username) {
         UserDetails userDetails = userService.loadUserByUsername(username);
         Algorithm algorithm = Algorithm.HMAC256(secretKey.getBytes());
-        return JWT.create()
+        String accessToken = JWT.create()
                 .withSubject(userDetails.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + Long.parseLong(refreshTokenDurationMS)))
+                .withExpiresAt(accessTokenExpiration())
                 .withClaim("role", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
+
+        String refreshToken = JWT.create()
+                .withSubject(userDetails.getUsername())
+                .withExpiresAt(refreshTokenExpiration())
+                .sign(algorithm);
+
+        return Map.of("access_token", accessToken, "refresh_token", refreshToken);
+    }
+
+    private Date accessTokenExpiration() {
+        return new Date(System.currentTimeMillis() + Integer.parseInt(accessTokenDurationMS));
+    }
+
+    private Date refreshTokenExpiration() {
+        return new Date(System.currentTimeMillis() + Long.parseLong(refreshTokenDurationMS));
     }
 }
