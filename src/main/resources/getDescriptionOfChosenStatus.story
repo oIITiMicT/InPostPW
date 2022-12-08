a596d76b589301ed9cf1ca7ab2a3fa51ecdd3b97
Vladislove228 <wlad.151@mail.ru>
2022-12-09 00:18:44 +0100

    3.0

---
+++ src/main/resources/getDescriptionOfChosenStatus.story
@@ -0,0 +1,7 @@
+Scenario: when a user provides correct id of parcel status , then 200 response code is returne
+
+Given a id of chosen status
+When the user send id of parcel status
+Then the user gets 200 response
+
+
--- src/test/java/getParcelStatusUS/GetParcelStatusSteps.java
+++ src/test/java/getParcelStatusUS/GetParcelStatusSteps.java
@@ -1,41 +1,32 @@
-package getInfoUserUS;
+package getParcelStatusUS;
+
 import com.example.InPostPW.dto.NewPackageFormDto;
-import com.example.InPostPW.dto.RegistrationFormDto;
-import com.example.InPostPW.services.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
-import org.jbehave.core.annotations.UsingSteps;
 import org.jbehave.core.annotations.When;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import static org.junit.jupiter.api.Assertions.fail;
-public class GetInfoUserSteps {
+public class GetParcelStatusSteps {
     private Long id;
     private RestTemplate restTemplate;
-
     private ObjectMapper mapper = new ObjectMapper();
     private ResponseEntity<String> resp;
-    private final static String URL = "http://localhost:8080/api/user/{id}";
 
-    private final static String JWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzYW1wbGVAZ21haWwuY29tIiwicm9sZSI6W10sImV4cCI6MTY3MDUyODI2M30.jTCM52MO2rXC1WzhVkTrN7KQzCmvXYaUv1TtisSHLXI";
-    private UserService userService;
-    @Given("a id")
-    public void initUserData() {
+    private final static String URL = "http://localhost:8080/api/{id}";
+    private static final String JWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzYW1wbGVAZ21haWwuY29tIiwiZXhwIjoxNjczMTE5MzYzfQ.kHs1vMq7B_cM7dX-rsMy3G_Djnit-mU6iDCGzlBognA";
 
+    @Given("a id of parcel")
+    public void initDataForParcel(){
         id = 1L ;
     }
 
-
-    @When("the user as a admin send id of user")
-    public void userSendsRequest() throws JsonProcessingException {
+    @When("the user send id of parcel")
+    public void sendRequest() throws JsonProcessingException {
         restTemplate = new RestTemplate();
 
 
@@ -48,11 +39,8 @@ public class GetInfoUserSteps {
                 URL, HttpMethod.GET, entity, String.class);
     }
 
-    @Then("the user as a admin gets 200 response")
-    public void userGets200Response() {
-
+    @Then("the user gets 200 response")
+    public void checkResponseStatus(){
         Assertions.assertEquals(HttpStatus.OK, resp.getStatusCode());
     }
-
-
 }
