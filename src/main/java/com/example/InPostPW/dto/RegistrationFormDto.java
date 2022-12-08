cba976d067905abd89c9b8a93deff6698160e136
ojAntoni <zezyulin90@gmail.com>
2022-12-08 21:55:26 +0300

    added not working JBehave warunek

--- src/main/java/com/example/InPostPW/dto/RegistrationFormDto.java
+++ src/main/java/com/example/InPostPW/dto/RegistrationFormDto.java
@@ -1,10 +1,14 @@
 package com.example.InPostPW.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RegistrationFormDto {
     private String username;
     private String password;
     private String email;
+
+
 }
--- src/main/java/com/example/InPostPW/services/UserService.java
+++ src/main/java/com/example/InPostPW/services/UserService.java
@@ -2,8 +2,11 @@ package com.example.InPostPW.services;
 
 import com.example.InPostPW.model.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
 import java.util.Optional;
 
+@Service
 public interface UserService extends UserDetailsService {
 
     Optional<User> findUserById(Long id);
